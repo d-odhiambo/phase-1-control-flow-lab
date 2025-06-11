305039bcb6215b1604600c6d41dd0523d763b15c
unknown <dancanodhiambo@gmail.com>
2025-06-11 10:13:39 +0300

    final lab

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
-function scuberGreetingForFeet(){
+const { expect } = require("chai")
+
+function scuberGreetingForFeet(distanceInFeet) {
+  if (distanceInFeet <= 400) {
+    return "This one is on me!";
+  } else if (distanceInFeet > 400 && distanceInFeet <= 2000) {
+    return "That will be twenty bucks.";
+  } else if (distanceInFeet > 2000 && distanceInFeet <= 2500) {
+    return "I will gladly take your thirty bucks.";
+  } else {
+    return "No can do.";
+  }
+  }
+  
+  // Write your code here!
+
+
+
+function ternaryCheckCity(destinationCity) {
+  return destinationCity === 'NYC' ? "Ok, sounds good." : "No go.";
   // Write your code here!
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function switchOnCharmFromTip(tipGiven) {
+  switch (tipGiven) {
+    case 'generous':
+      return "Thank you so much.";
+    case 'not as generous':
+      return "Thank you.";
+    default:
+      return "Bye.";
+  }
 }
-
-function switchOnCharmFromTip(){
   // Write your code here!
-}
